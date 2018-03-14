6c986c8205fbeaf87e471b292dff64caabc6eefa
Steve Springett <steve@springett.us>
2018-03-14 11:57:46 -0500

    Layout and content changes

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="Dependency-Track is an intelligent Software Composition Analysis (SCA) platform that allows organizations to automatically ingest and identify third-party components and any inherited vulnerabilities from their use."/>
-        <meta name="keywords" content="sdlc, devops, automation, software, bill of materials, owasp, appsec, security, bom, vulnerabilities, visibility, component analysis, nvd, nist, software security, dependency check, software composition analysis, sca, supply chain risk management, scrm, c-scrm, cybersecurity, node security platform, nsp, vulndb"/>
+        <meta name="description" content="Dependency-Track is an intelligent Component Analysis platform that allows organizations to automatically ingest and identify third-party components and any inherited vulnerabilities from their use."/>
+        <meta name="keywords" content="sdlc, devops, devsecops, automation, software, bill of materials, owasp, appsec, security, bom, vulnerabilities, visibility, component analysis, nvd, nist, software security, dependency check, software composition analysis, sca, supply chain risk management, scrm, c-scrm, cybersecurity, node security platform, nsp, vulndb"/>
         <meta name="dcterms.rightsHolder" content="Steve Springett">
         <meta name="dcterms.dateCopyrighted" content="<?php echo date("Y"); ?>">
         <meta name="robots" content="index,follow"/>
@@ -16,8 +16,8 @@
         <link rel="shortcut icon" type="image/ico" href="/favicon.ico"/>
         <link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="assets/css/animate.min.css">
-        <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
-        <link rel="stylesheet" type="text/css" href="assets/css/fontcustom.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/fontawesome-all.min.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/font-mfizz.css">
         <link rel="stylesheet" type="text/css" href="assets/owl-carrosel/owl.carousel.min.css">
         <link rel="stylesheet" type="text/css" href="assets/slick-slider/slick.css">
         <link rel="stylesheet" type="text/css" href="assets/slick-slider/slick-theme.css">
@@ -33,7 +33,7 @@
             <nav class="navbar navbar-inverse hidden-sm hidden-xs">
                 <div class="container">
                     <div class="navbar-header">
-                        <a class="navbar-brand" href="index.php"><img src="assets/images/logo-greyscale.svg" alt="Dependency-Track Logo"></a>
+                        <a class="navbar-brand page-scroll" href="#home"><img src="assets/images/logo-greyscale.svg" alt="Dependency-Track Logo"></a>
                     </div>
                     <div class="collapse navbar-collapse navbar-right">
                         <ul class="nav navbar-nav">
@@ -58,13 +58,13 @@
                     <div class="col-md-5 col-sm-9">
                         <div class="wrapper-content">
                             <span class="small-text"><span class="color-text">Reduce </span> Open Source Risk</span>
-                            <h3>DevOps-Accelerated Component Analysis</h3>
-                            <a href="#download">
-                                <button class="btn btn-default">Download <i class="fa fa-download" aria-hidden="true"></i></button>
+                            <h3>Continuous Component Analysis Platform</h3>
+                            <a class="page-scroll" href="#download">
+                                <button class="btn btn-default">Download <i class="fas fa-download" aria-hidden="true"></i></button>
                             </a>
                             <span class="spacer"></span>
                             <a href="https://docs.dependencytrack.org/">
-                                <button class="btn btn-default">Documentation <i class="fa fa-book" aria-hidden="true"></i></button>
+                                <button class="btn btn-default">Documentation <i class="fas fa-book" aria-hidden="true"></i></button>
                             </a>
                         </div>
                     </div>
@@ -101,7 +101,7 @@
                     </div>
                     <div class="col-md-3 col-md-offset-1 col-sm-5">
                         <div  class="right-bar">
-                            <h3>Discover how third-party components can introduce risk</h3>
+                            <h3>Discover how third-party components introduce risk</h3>
                             <div id="lightBox" class="v-text">
                                 <a href="http://youtu.be/zlruKS4VsWQ" data-poptrox="youtube,800x480"><i class="fa fa-play" aria-hidden="true"></i> Watch how it works</a>
                             </div>
@@ -131,10 +131,10 @@
                 <div class="title">
                     <div class="row">
                         <div class="col-sm-6">
-                            <h2>Promotes Secure Continuous Delivery Pipelines</h2>
+                            <h2>Continuous Component Analysis</h2>
                         </div>
                         <div class="col-sm-6">
-                            <p>Dependency-Track is an intelligent Software Composition Analysis (SCA) platform that allows organizations to quickly identify third-party and open source components that are placing your applications at risk.</p>
+                            <p>Dependency-Track is an intelligent Component Analysis platform that allows organizations to quickly identify third-party and open source components that are placing your applications at risk.</p>
                         </div>
                     </div>
                 </div>
@@ -142,46 +142,60 @@
                     <div class="row first-row">
                         <div class="col-sm-4">
                             <div class="cta-wrapper">
-                                <i class="fa fa-th-large" aria-hidden="true"></i>
-                                <h3><a href="#">Apps and Notifications</a></h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
+                                <h3><i class="fas fa-chart-bar" aria-hidden="true"></i> <a href="#">Dashboard</a></h3>
+                                <p>Provides high-level metrics and trends on the portfolio and projects that are impacting an organizations risks</p>
                             </div>
                         </div>
                         <div class="col-sm-4">
                             <div class="cta-wrapper">
-                                <i class="fa fa-cog" aria-hidden="true"></i>
-                                <h3><a href="#">Customizable watch Faces</a></h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
+                                <h3><i class="fas fa-cogs" aria-hidden="true"></i> <a href="#">Continuous Delivery</a></h3>
+                                <p>Excels in rapidly changing CI/CD environments by encouraging ingestion and analysis of continuously updated components</p>
                             </div>
                         </div>
                         <div class="col-sm-4">
                             <div class="cta-wrapper">
-                                <i class="fa fa-compass" aria-hidden="true"></i>
-                                <h3><a href="#">Built-in GPS</a></h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
+                                <h3><i class="fas fa-crosshairs" aria-hidden="true"></i> <a href="#">Rapid Response</a></h3>
+                                <p>Identifies vulnerabilities in all projects and applications which are affected from the use of vulnerable components</p>
                             </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-sm-4">
                             <div class="cta-wrapper">
-                                <i class="fa fa-signal" aria-hidden="true"></i>
-                                <h3><a href="#">Third-Party Health Apps</a></h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
+                                <h3><i class="fas fa-th" aria-hidden="true"></i> <a href="#">Supply Chain Risk</a></h3>
+                                <p>Expands traditional Software Composition Analysis (SCA) by recognizing hardware/IoT as components with potential vulnerabilities</p>
                             </div>
                         </div>
                         <div class="col-sm-4">
                             <div class="cta-wrapper">
-                                <i class="fa fa-repeat" aria-hidden="true"></i>
-                                <h3><a href="#">Activity Rings</a></h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
+                                <h3><i class="fas fa-globe" aria-hidden="true"></i> <a href="#">API and Integration</a></h3>
+                                <p>Well documented API-first design integrates easily with other systems providing endless possibilities</p>
                             </div>
                         </div>
                         <div class="col-sm-4">
                             <div class="cta-wrapper last-wrapper">
-                                <i class="fa fa-heart-o" aria-hidden="true"></i>
-                                <h3><a href="#">Heart Rate Sensor</a></h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
+                                <h3><i class="fas fa-database" aria-hidden="true"></i> <a href="#">Vulnerability Datasources</a></h3>
+                                <p>Mirrors data from multiple sources of vulnerability intelligence providing more coverage on a wider range of components</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <div class="cta-wrapper">
+                                <h3><i class="fab fa-osi" aria-hidden="true"></i> <a href="#">Open Source</a></h3>
+                                <p>Community-driven project which encourages continuous improvement and is distributed under the Apache 2.0 license <i class="icon-apache"></i></p>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="cta-wrapper">
+                                <h3><i class="fas fa-archive" aria-hidden="true"></i> <a href="#">Bill of Materials (BoM)</a></h3>
+                                <p>Supports automatic ingestion of CycloneDX and SPDX BoM formats along with Dependency-Check reports</p>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="cta-wrapper last-wrapper">
+                                <h3><i class="fas fa-server" aria-hidden="true"></i> <a href="#">Enterprise Integrations</a></h3>
+                                <p>Supports Active Directory (via LDAP) for authentication and multiple commercial and open source database engines</p>
                             </div>
                         </div>
                     </div>
@@ -191,28 +205,32 @@
 
         <section id="download" class="download-section">
             <div class="container">
-                <div class="col-md-6">
+                <div class="col-md-12">
                     <div class="wrapper-content">
-                        <span class="small">Dependency-Track is free and Apache 2.0 licensed</span><br/><br/>
                         <div>
-                            <span class="caption vertically-centered"><i class="fa fa-docker download-icon" aria-hidden="true" title="Docker"></i></span>
+                            <span class="caption vertically-centered"><i class="icon-docker download-icon" aria-hidden="true" title="Docker"></i></span>
                             <span class="docker-command vertically-centered">docker pull owasp/dependency-track</span>
                         </div>
+                        <br/>
                         <div>
-                            <span class="caption vertically-centered"><i class="fa fa-download download-icon" aria-hidden="true" title="Download"></i></span>
-                            <span class="download-option vertically-centered">Java WARs available on  <a href="https://github.com/stevespringett/dependency-track/releases">GitHub</a></span>
+                            <span class="caption vertically-centered"><i class="icon-jetty download-icon"></i></span>
+                            <span class="download-option vertically-centered"><a href="https://github.com/stevespringett/dependency-track/releases">Download <i class="fas fa-download"></i> self-contained application (Requires Java)</a></span>
+                        </div>
+                        <br/>
+                        <div>
+                            <span class="caption vertically-centered"><i class="icon-tomcat download-icon"></i></span>
+                            <span class="download-option vertically-centered"><a href="https://github.com/stevespringett/dependency-track/releases">Download <i class="fas fa-download"></i> Java webapp (Requires Tomcat 8.5+ or Jetty 9.0+)</a></span>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-6">
-                    <div class="wrapper-content text-right">
-                        <span class="small">Connect with the developers &amp; community:</span><br/><br/>
-                        <a href="https://github.com/stevespringett/dependency-track"><i class="fa fa-github social-icon" aria-hidden="true" title="GitHub"></i></a>
-                        <a href="https://twitter.com/dependencytrack"><i class="fa fa-twitter social-icon" aria-hidden="true" title="Twitter"></i></a>
-                        <a href="https://www.youtube.com/channel/UC8xdttysl3gNAQYvk1J9Efg"><i class="fa fa-youtube-play social-icon" aria-hidden="true" title="YouTube"></i></a>
-                        <a href="https://owasp.slack.com/messages/proj-dependency-track"><i class="fa fa-slack social-icon" aria-hidden="true" title="Slack"></i></a>
-                        <a href="https://gitter.im/dependency-track/Lobby?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge"><i class="fa fa-comments social-icon" aria-hidden="true" title="Gitter"></i></a>
-                    </div>
+            </div>
+            <div class="social">
+                <div class="title text-center">
+                    <a href="https://github.com/stevespringett/dependency-track"><i class="fab fa-github social-icon" aria-hidden="true" title="GitHub"></i></a>
+                    <a href="https://twitter.com/dependencytrack"><i class="fab fa-twitter social-icon" aria-hidden="true" title="Twitter"></i></a>
+                    <a href="https://www.youtube.com/channel/UC8xdttysl3gNAQYvk1J9Efg"><i class="fab fa-youtube social-icon" aria-hidden="true" title="YouTube"></i></a>
+                    <a href="https://owasp.slack.com/messages/proj-dependency-track"><i class="fab fa-slack-hash social-icon" aria-hidden="true" title="Slack"></i></a>
+                    <a href="https://gitter.im/dependency-track/Lobby?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge"><i class="fab fa-gitter social-icon" aria-hidden="true" title="Gitter"></i></a>
                 </div>
             </div>
         </section>
