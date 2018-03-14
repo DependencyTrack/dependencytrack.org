149548033508e37e6db1d9d3d608a9e33610fc55
Steve Springett <steve@springett.us>
2018-03-14 16:30:02 -0500

    scroll speed

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ jQuery(function ($) {
                 var $anchor = $(this);
                 $('html, body').stop().animate({
                     scrollTop: $($anchor.attr('href')).offset().top
-                }, 700, 'easeInOutExpo');
+                }, 1000, 'easeInOutExpo');
                 event.preventDefault();
             });
         });
